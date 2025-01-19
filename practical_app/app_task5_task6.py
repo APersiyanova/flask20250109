fa78c75710f272a67ab77ca0ef26d763a9b390cd
APersiyanova <wrocla@mail.ru>
2025-01-19 20:08:55 +0300

    task6 completed

--- practical_app/app_task5_task6.py
+++ practical_app/app_task5_task6.py
@@ -3,6 +3,7 @@
 разделённых слешем /. Endpoint должен вернуть текст «Максимальное переданное число {number}»'''
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -12,6 +13,20 @@ def max_number(numbers):
     numbers = list(map(int, numbers))
     max_number = max(numbers)
     return f'Максимальное переданное число - <i>{str(max_number)}</i>'
+
+# Задача 6. Превью файла
+'''Реализуйте endpoint, который показывает превью файла, принимая на вход два параметра: SIZE (int) 
+и RELATIVE_PATH — и возвращая первые SIZE символов файла по указанному в RELATIVE_PATH пути'''
+
+@app.route('/<int:size>/<path:relative_path>')
+def show_content(size, relative_path):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    abs_path = os.path.join(base_dir,relative_path)
+    with open(relative_path) as file:
+        # result_text = file.read(size)
+        result_text = file.read()[:size]
+        result_size = len(result_text)
+        return f'Из файла <i>{abs_path}</i> прочитано {result_size} символов:<br><br><b>{result_text}</b>'
 
 
 if __name__ == '__main__':
